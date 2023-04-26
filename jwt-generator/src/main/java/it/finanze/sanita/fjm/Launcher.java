b83e83353e7dd49dc2e07d704111437818994256
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2023-04-26 15:36:58 +0200

    Update Launcher.java

--- jwt-generator/src/main/java/it/finanze/sanita/fjm/Launcher.java
+++ jwt-generator/src/main/java/it/finanze/sanita/fjm/Launcher.java
@@ -24,6 +24,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
