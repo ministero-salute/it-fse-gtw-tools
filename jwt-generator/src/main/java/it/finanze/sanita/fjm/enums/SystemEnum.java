49b210108357ccbef0ad5c241c51b682d09c4c89
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2023-06-27 12:07:03 +0200

    Update SystemEnum.java

--- jwt-generator/src/main/java/it/finanze/sanita/fjm/enums/SystemEnum.java
+++ jwt-generator/src/main/java/it/finanze/sanita/fjm/enums/SystemEnum.java
@@ -37,7 +37,7 @@ public enum SystemEnum {
 	/**
 	 * Terminology.
 	 */
-	TERMINOLOGY("terminology","FSE-JWT-Authoring");
+	TERMINOLOGY("terminology","FSE-JWT-Terminology");
 	
 	/**
 	 * Key argument.
